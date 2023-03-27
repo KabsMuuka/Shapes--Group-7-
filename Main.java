bde7f66db5c83629b8d0efd5d7644e4b03c26ac3
Kabsmuuka <kabwalimuuka@gmail.com>
2023-03-27 18:32:57 +0200

    I have created the main class And the Shapes class. Child classes will extend Shapes

---
+++ Main.java
@@ -0,0 +1,67 @@
+package org.example;
+
+public class Main {
+    public static void main(String[]args){
+
+    //Instances of the specific shapes
+    Shapes circle = new Circle("Circle","Triangle","Rectangle");
+    Shapes triangle = new Triangle("Circle","Triangle","Rectangle");
+    Shapes rectangle = new Rectangle("Circle","Triangle","Rectangle");
+
+
+        //This Calls the Circle Class
+        System.out.println(circle.getCircle());
+        //Area of the Circle
+        circle.Area();
+        circle.WhichShape();
+        
+        //This Calls the Triangle Class 
+        System.out.println("*****************************************************************");
+        System.out.println(triangle.getTriangle());
+        //Area of the Triangle
+        triangle.Area();
+        triangle.WhichShape();
+        
+
+        //This Calls the Rectangle class
+        System.out.println("*****************************************************************");
+        System.out.println(rectangle.getRectangle());
+        //Area of Rectangle
+        rectangle.Area(); 
+        rectangle.WhichShape();
+       
+       
+}
+
+}
+//Shapes Class => Parent Class
+abstract class Shapes {
+    private String Circle;
+    private String Triangle;
+    private String Rectangle;
+
+    //constructor of the Shape
+    Shapes(String Circle,String Triangle,String Rectangle){
+        this.Circle = Circle;
+        this.Triangle = Triangle;
+        this.Rectangle = Rectangle;
+    }
+    //Area of the Shape 
+    void Area(){
+
+    }
+    //Should print out the name of the shape! and other
+    void WhichShape(){
+
+    }
+    String getCircle(){
+        return Circle;
+    }
+    String getTriangle(){
+        return Triangle;
+    }
+    String getRectangle(){
+        return Rectangle;
+    }
+}
+
