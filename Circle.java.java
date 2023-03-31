59843ff1ed4aefdfd9910132fd59ad87b6fb9ecb
Gabriel18711 <122276140+Gabriel18711@users.noreply.github.com>
2023-03-31 11:43:21 +0200

    Add files via upload

---
+++ Circle.java.java
@@ -0,0 +1,8 @@
+import java.lang.Math;
+
+public class Circle {
+    private double radius; // the radius of the circle
+    
+    // constructor that takes in a radius value
+    public Circle(double radius) {
+        this
