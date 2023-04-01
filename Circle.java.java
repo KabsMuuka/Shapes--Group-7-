c4bbbf5ed75930bb52c2a91367ad438e3af913f3
Gabriel18711 <122276140+Gabriel18711@users.noreply.github.com>
2023-04-01 16:59:02 +0200

    Update Circle.java.java

--- Circle.java.java
+++ Circle.java.java
@@ -5,4 +5,32 @@ public class Circle {
     
     // constructor that takes in a radius value
     public Circle(double radius) {
-        this
+        this.radius = radius;
+    }
+    
+    // getter method for the radius property
+    public double getRadius() {
+        return radius;
+    }
+    
+    // setter method for the radius property
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    
+    // method that calculates the area of the circle
+    public double calculateArea() {
+        return Math.PI * Math.pow(radius, 2);
+    }
+    
+    // method that calculates the circumference of the circle
+    public double calculateCircumference() {
+        return 2 * Math.PI * radius;
+    }
+    
+    // method that returns a string representation of the circle object
+    @Override
+    public String toString() {
+        return "Circle with radius " + radius;
+    }
+}
